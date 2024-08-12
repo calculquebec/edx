899ad13602ba2da47edbbd14afff8853b2d82fb3
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-08-12 16:00:30 -0400

    Revert "Revert "add terraform block to pin cloudflare version""
    This reverts commit 49271ee59e80ccb9fa62fdc6a9f9c32fa22e77d5.

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.4.0"
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.38.0"
+    }
+  }
+}
+
 variable "pool" {
   description = "Slurm pool of compute nodes"
   default = []
