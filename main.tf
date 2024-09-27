c380e8035c0f44d97c649cb4e2223c6657081e12
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-09-27 10:23:33 -0400

    add edx vhosts

--- main.tf
+++ main.tf
@@ -86,6 +86,7 @@ module "dns" {
    public_instances = module.openstack.public_instances
    ssh_private_key  = module.openstack.ssh_private_key
    sudoer_username  = module.openstack.accounts.sudoer.username
+   vhosts           = ["edx", "ipa", "jupyter", "mokey", "explore"]
 }
 
 ## Uncomment to register your domain name with Google Cloud
