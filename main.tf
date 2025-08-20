d5fd51ea69445bd25034b0f2a12d4cf07d901653
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-08-20 10:54:32 -0400

    moved to wider subnet

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ module "openstack" {
   software_stack = "computecanada"
   eyaml_key = base64decode(var.eyaml_key)
 
-  subnet_id = "f7412a24-e802-4a72-8e1f-f74bac4a0b5a"
+  subnet_id = "a7f9fef1-a43e-4502-83a9-e47c936b635d"
   os_ext_network = "Public-Network"
 
   puppetfile = file("Puppetfile")
