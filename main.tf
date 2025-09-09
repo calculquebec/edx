a7d42d55b6f05f210ed36cdc99d404414b3f12c8
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-09-09 09:31:46 -0400

    keep mgmt1 with ssd after all

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "openstack" {
   image        = "AlmaLinux-9"
 
   instances = {
-    mgmt   = { type = "p4-7.5gb", tags = ["puppet", "mgmt", "nfs"], count = 1, disk_size=100, disk_type="volumes-ec"}
+    mgmt   = { type = "p4-7.5gb", tags = ["puppet", "mgmt", "nfs"], count = 1, disk_size=100 }
     login  = { type = "p4-7.5gb", tags = ["login", "public", "proxy"], count = 1}
     node   = { type = "c2-7.5gb", tags = ["node"], count = 1 }
     nodepool   = { type = "c2-7.5gb", tags = ["node", "pool"], count = 1, image = "snapshot-cpunode-2025.3-A9.6" }
