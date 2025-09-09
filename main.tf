43286776693537e7669cc0c08fd0e68510adc0cc
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-09-09 09:22:03 -0400

    keep login as ssd

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ module "openstack" {
 
   instances = {
     mgmt   = { type = "p4-7.5gb", tags = ["puppet", "mgmt", "nfs"], count = 1, disk_size=100, disk_type="volumes-ec"}
-    login  = { type = "p4-7.5gb", tags = ["login", "public", "proxy"], count = 1, disk_type="volumes-ec"}
+    login  = { type = "p4-7.5gb", tags = ["login", "public", "proxy"], count = 1}
     node   = { type = "c2-7.5gb", tags = ["node"], count = 1 }
     nodepool   = { type = "c2-7.5gb", tags = ["node", "pool"], count = 1, image = "snapshot-cpunode-2025.3-A9.6" }
     evolo = { type = "p2-3.75gb", tags = ["internal_login"], count = 1, disk_type="volumes-ec"}
