e68b98afce7851276be4be6add0ca78998153ff9
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-12-04 11:36:28 -0500

    split caddy and jupyter1

--- main.tf
+++ main.tf
@@ -89,7 +89,8 @@ module "openstack" {
     mgmt   = { type = "p4-7.5gb", tags = ["mgmt", "nfs", "mgmt_extra"], count = 1, disk_size=100 }
     puppet = { type = "p4-7.5gb", tags = ["puppet"], count = 1 }
     login  = { type = "p4-7.5gb", tags = ["login", "public"], count = 1}
-    jupyter = { type = "p4-7.5gb", tags = ["public", "proxy"], count = 1}
+    caddy = { type = "p2-3.75gb", tags = ["public", "proxy"], count = 1}
+    jupyter = { type = "p2-3.75gb", tags = ["public", "jupyterhub"], count = 1}
     cip101- = { type = "c2-7.5gb", tags = ["node", "pool"], feature = ["cip101"], image = "snapshot-cpunode-2025.3-A9.6", count = 10 }
     node   = { type = "c2-7.5gb", tags = ["node"], count = 1 }
     nodepool   = { type = "c2-7.5gb", tags = ["node", "pool"], count = 1, image = "snapshot-cpunode-2025.3-A9.6" }
