aa03de75458d16b73b98866bd3aff45f46e068ae
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-12-04 10:50:22 -0500

    split login and jupyter

--- main.tf
+++ main.tf
@@ -52,7 +52,8 @@ module "openstack" {
 
   instances = {
     mgmt   = { type = "p4-7.5gb", tags = ["puppet", "mgmt", "nfs"], count = 1, disk_size=100 }
-    login  = { type = "p4-7.5gb", tags = ["login", "public", "proxy"], count = 1}
+    login  = { type = "p4-7.5gb", tags = ["login", "public"], count = 1}
+    jupyter = { type = "p4-7.5gb", tags = ["public", "proxy"], count = 1}
     node   = { type = "c2-7.5gb", tags = ["node"], count = 1 }
     nodepool   = { type = "c2-7.5gb", tags = ["node", "pool"], count = 1, image = "snapshot-cpunode-2025.3-A9.6" }
     evolo = { type = "p2-3.75gb", tags = ["internal_login"], count = 1, disk_type="volumes-ec"}
