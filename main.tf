13e55bd558889f81805250b868f6d514dc4ac932
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2026-08-19 11:00:35 -0400

    bump to MC pre-16

--- main.tf
+++ main.tf
@@ -142,9 +142,9 @@ locals {
 }
 
 module "openstack" {
-  source         = "git::https://github.com/computecanada/magic_castle.git//openstack?ref=15.6.1"
+  source         = "git::https://github.com/computecanada/magic_castle.git//openstack?ref=4ae5ab9"
   config_git_url = "https://github.com/computecanada/puppet-magic_castle.git"
-  config_version = "6eb919e"
+  config_version = "7e03ce0"
 
   cluster_name = "evolo${var.suffix}"
   domain       = "calculquebec.cloud"
@@ -209,15 +209,15 @@ output "public_ip" {
 }
 
 ## Uncomment to register your domain name with CloudFlare
-#module "dns" {
-#   source           = "git::https://github.com/computecanada/magic_castle.git//dns/cloudflare?ref=15.6.1"
-#   name             = module.openstack.cluster_name
-#   domain           = module.openstack.domain
-#   public_instances = module.openstack.public_instances
-#   domain_tag       = "proxy"
-#   vhosts           = ["*.edx", "edx", "ipa", "jupyter", "mokey", "explore", "metrix"]
-#   dkim_public_key  = file("keys/dkim_public.pem")
-#}
+module "dns" {
+   source           = "git::https://github.com/computecanada/magic_castle.git//dns/cloudflare?ref=4ae5ab9"
+   name             = module.openstack.cluster_name
+   domain           = module.openstack.domain
+   public_instances = module.openstack.public_instances
+   domain_tag       = "proxy"
+   vhosts           = ["*.edx", "edx", "ipa", "jupyter", "mokey", "explore", "metrix"]
+   dkim_public_key  = file("keys/dkim_public.pem")
+}
 
 ## Uncomment to register your domain name with Google Cloud
 # module "dns" {
